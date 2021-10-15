99272e1b5a0fa421cdc77a55565ad59d2f4ca452
StamateMihaiDaniel <stamate.mihaidaniel@yahoo.com>
2021-10-16 00:06:39 +0300

    Success

--- src/main/java/com/onlineStore/springboot/controller/ProductController.java
+++ src/main/java/com/onlineStore/springboot/controller/ProductController.java
@@ -12,7 +12,7 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-    //Display list of all products\
+
     @RequestMapping("/")
     public String viewHomePage(Model model){
         model.addAttribute("listProducts", productService.getAllProducts());
@@ -21,7 +21,7 @@ public class ProductController {
 
     @GetMapping("/showNewProductForm")
     public String showNewProductForm(Model model){
-        //create model attribute to bind form data
+
         Product product = new Product();
         model.addAttribute("product", product);
         return "newProduct";
@@ -35,10 +35,10 @@ public class ProductController {
 
     @GetMapping("/showFormForUpdate/{id}")
     public String showFormForUpdate(@PathVariable(value = "id") long id, Model model){
-        // get employee from the service
+
         Product product = productService.getProductById(id);
 
-        //set employee as a model attribute to pre-populate the form
+
         model.addAttribute("product", product);
         return "update_product";
 
@@ -46,7 +46,7 @@ public class ProductController {
 
     @GetMapping("/deleteProduct/{id}")
     public String deleteEmployee(@PathVariable (value = "id") long id){
-        //call delete employee method
+
         this.productService.deleteProductById(id);
         return "redirect:/";
 
